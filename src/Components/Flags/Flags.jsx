358e0bbcb6029f18bb6b132d8dcc9e4271db57fd
tery999 <51335465+tery999@users.noreply.github.com>
2024-04-21 19:56:46 +0300

    testing

--- src/Components/Flags/Flags.jsx
+++ src/Components/Flags/Flags.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom"
+
 export default function Flags() {
     return (
         <div>
-            <div>Europe</div>
-            <div>Asia</div>
-            <div>Americas</div>
-            <div>Africa</div>
+            <Link to="Europe"><div>Europe</div></Link>
+            <Link to="Asia"><div>Asia</div></Link>
+            <Link to="Americas"><div>Americas</div></Link>
+            <Link to="Africa"><div>Africa</div></Link>
         </div>
     )
 }
