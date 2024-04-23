3c5303df9481e0fb44c0886b1353a416fcfc1988
tery999 <51335465+tery999@users.noreply.github.com>
2024-04-23 15:18:54 +0300

    flag links images

--- src/Components/Flags/Flags.jsx
+++ src/Components/Flags/Flags.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
+import * as styles from "./Flags.module.css"
 
 export default function Flags() {
     return (
-        <div>
-            <Link to="Europe"><div>Europe</div></Link>
-            <Link to="Asia"><div>Asia</div></Link>
-            <Link to="Americas"><div>Americas</div></Link>
-            <Link to="Africa"><div>Africa</div></Link>
+        <div className={styles.container}>
+             <Link to="Europe"><div className={styles.Europe}>Europe</div></Link>
+             <Link to="Asia"><div className={styles.Asia}>Asia</div></Link>
+             <Link to="Americas"><div className={styles.America}>Americas</div></Link>
+             <Link to="Africa"><div className={styles.Africa}>Africa</div></Link>
         </div>
     )
 }
