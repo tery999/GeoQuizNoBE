14f8423b45852b04fb77cbfee81f50a7b8ed1ae6
tery999 <51335465+tery999@users.noreply.github.com>
2024-04-23 09:19:28 +0300

    Css
    Added link images

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+import * as styles from "./Home.module.css"
 
 export default function Home() {
     return (
-        <div>
-             <Link to="Flags"><div>Flags</div></Link>
-             <Link to="Outlines"><div>Outlines</div></Link>
-             <Link to="Borders"><div>Borders</div></Link>
+        <div className={styles.container}>
+             <Link to="Flags"><div className={styles.flagDiv}>Flags</div></Link>
+             <Link to="Outlines"><div className={styles.OutDiv}>Outlines</div></Link>
+             <Link to="Capitals"><div className={styles.CapDiv}>Capitals</div></Link>
         </div>
     )
 }
--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -4,9 +4,10 @@ import * as styles from "./Navigation.module.css"
 export default function Navigation () {
     return (
         <div className={styles.NavContainer}>
+            <Link to="/"><div>Home</div></Link>
         <Link to="Flags"><div className={styles.DivLinks}>Flags</div></Link>
              <Link to="Outlines"><div>Outlines</div></Link>
-             <Link to="Borders"><div>Borders</div></Link>
+             <Link to="Capitals"><div>Capitals</div></Link>
         </div>
     )
 }
