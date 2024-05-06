529211d9985687f3ec7b210d03d4562cc2ee3ade
tery999 <51335465+tery999@users.noreply.github.com>
2024-05-06 11:35:22 +0300

    fixed problem
    Wanted to wait for picture to load before enabling buttons. But changing the state would trigger re-rendering which would make the choices flicker and refresh, making it look somewhat strange.
    
    Moved the choice login in an IF statement, providing an empty array as initializer.

--- src/Components/Outlines/AmericasOutlines/AmericasOutlines.jsx
+++ src/Components/Outlines/AmericasOutlines/AmericasOutlines.jsx
@@ -1,17 +1,20 @@
 import { useEffect, useRef, useState } from 'react'
 import * as styles from "./AmericasOutlines.module.css"
-import {useCustomOutlines} from "../../../Services/useCustomOutlines"
+import { useCustomOutlines } from "../../../Services/useCustomOutlines"
 import { getAmericaFlags } from '../../../Services/Services';
 
 
-export default function AmericasOutlines () {
-    //using custom hook, doesnt look pretty, need to make it better
-    const [ countryArrShuffled ,choices, 
-        setChoices, currentOutline, setCurrentOutline, 
-        loaded, correctAsnwers, totalAnswers, currentTurn] = useCustomOutlines(getAmericaFlags);
-        
-      const [imageHasLoaded, setImageHasLoaded] = useState(false);
-      
+export default function AmericasOutlines() {
+  //using custom hook, doesnt look pretty, need to make it better
+  const [countryArrShuffled, choices,
+    setChoices, currentOutline, setCurrentOutline,
+    loaded, correctAsnwers, totalAnswers, currentTurn] = useCustomOutlines(getAmericaFlags);
+
+  const [imageHasLoaded, setImageHasLoaded] = useState(false);
+
+  let fourChoicesShuffled = [];
+  //show no buttons if image hasnt loaded yet + prevents double rendering
+  if (imageHasLoaded === true) {
     const fourChoices = [];
     fourChoices.push(currentOutline);
     const choicesWithoutCurrent = choices.filter((country) => {
@@ -20,51 +23,52 @@ export default function AmericasOutlines () {
     const choicesWithoutCurrentShuffled = choicesWithoutCurrent.sort(() => Math.random() - 0.5);
     const threeLeft = choicesWithoutCurrentShuffled.slice(0, 3);
     fourChoices.push(...threeLeft);
-    const fourChoicesShuffled = fourChoices.sort(() => Math.random() - 0.5);
-  
-    const choiceClickFunction = (name) => {
-      if (name === currentOutline.name) {
-        correctAsnwers.current++;
-      }
-      const newCurOutline = countryArrShuffled.splice(0, 1)[0];
-      currentTurn.current++;
-      setCurrentOutline(newCurOutline);
-      setImageHasLoaded(false);
-    }
-  
-    const ResetFunction = () => {
-      window.location.reload();
-    }
-  
-    const currentOutlineName = currentOutline?.name;
-    return (
-      <div className={styles.container}>
-        {currentTurn.current <= totalAnswers.current &&
-          <div> {currentTurn.current}/{totalAnswers.current}</div>
-        }
-        {loaded && currentOutline &&
-          <>
-            <div className={styles.flagContainer}> <img className={[styles.flagImg, styles[currentOutlineName]].join(' ')} src={currentOutline.OutlineURL} 
-            alt="" onLoad={() => setImageHasLoaded(true)} /></div>
-            <div className={styles.Options}>
-              {fourChoicesShuffled.map((oneOption => {
-                return <button key={oneOption._id[0]} disabled={!imageHasLoaded} onClick={() => choiceClickFunction(oneOption.name)}> {oneOption.name}</button>
-              }))}
-  
-            </div>
-          </>
-        }
-        {!currentOutline && correctAsnwers.current !== totalAnswers.current &&
-          <div className={styles.FinalScore}>Your score is {correctAsnwers.current} out of {totalAnswers.current}</div>
-        }
-        {!currentOutline && correctAsnwers.current === totalAnswers.current &&
-          <div className={styles.FinalScore}>Perfect score</div>
-        }
-  
-        {!currentOutline &&
-          <button className={styles.tryAgain} onClick={ResetFunction}> Try again </button>
-        }
-      </div>
-    )
+    fourChoicesShuffled = fourChoices.sort(() => Math.random() - 0.5);
   }
- 
+
+  const choiceClickFunction = (name) => {
+    if (name === currentOutline.name) {
+      correctAsnwers.current++;
+    }
+    const newCurOutline = countryArrShuffled.splice(0, 1)[0];
+    currentTurn.current++;
+    setCurrentOutline(newCurOutline);
+    setImageHasLoaded(false);
+  }
+
+
+  const ResetFunction = () => {
+    window.location.reload();
+  }
+
+  const currentOutlineName = currentOutline?.name;
+  return (
+    <div className={styles.container}>
+      {currentTurn.current <= totalAnswers.current &&
+        <div> {currentTurn.current}/{totalAnswers.current}</div>
+      }
+      {loaded && currentOutline &&
+        <>
+          <div className={styles.flagContainer}> <img className={[styles.flagImg, styles[currentOutlineName]].join(' ')} src={currentOutline.OutlineURL}
+            alt="" onLoad={() => setImageHasLoaded(true)} /></div>
+          <div className={styles.Options}>
+            {fourChoicesShuffled.map((oneOption => {
+              return <button key={oneOption._id[0]} disabled={!imageHasLoaded} onClick={() => choiceClickFunction(oneOption.name)}> {oneOption.name}</button>
+            }))}
+
+          </div>
+        </>
+      }
+      {!currentOutline && correctAsnwers.current !== totalAnswers.current &&
+        <div className={styles.FinalScore}>Your score is {correctAsnwers.current} out of {totalAnswers.current}</div>
+      }
+      {!currentOutline && correctAsnwers.current === totalAnswers.current &&
+        <div className={styles.FinalScore}>Perfect score</div>
+      }
+
+      {!currentOutline &&
+        <button className={styles.tryAgain} onClick={ResetFunction}> Try again </button>
+      }
+    </div>
+  )
+}
