72c9df18dead2b6d7c6677d854a2414e69a0d99e
tery999 <51335465+tery999@users.noreply.github.com>
2024-05-14 17:04:17 +0300

    ideas how to place to Plus categories

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom"
 import * as styles from "./Navigation.module.css"
 
-export default function Navigation () {
+export default function Navigation() {
     return (
         <div className={styles.NavContainer}>
             <Link to="/"><div>Home</div></Link>
-        <Link to="Flags"><div className={styles.DivLinks}>Flags</div></Link>
-             <Link to="Outlines"><div>Outlines</div></Link>
-             <Link to="Capitals"><div>Capitals</div></Link>
+            <div className={styles.BothVersions}>
+                <Link to="Flags"><div className={styles.DivLinks}>Flags</div></Link>
+                <Link to="Flags"><div className={styles.DivLinksPlus}>+</div></Link>
+            </div>
+            <div className={styles.BothVersions}>
+                <Link to="Outlines"><div className={styles.DivLinks}>Outlines</div></Link>
+                <Link to="Outlines"><div className={styles.DivLinksPlus}>+</div></Link>
+            </div>
+            <div className={styles.BothVersions}>
+                <Link to="Capitals"><div className={styles.DivLinks}>Capitals</div></Link>
+                <Link to="Capitals"><div className={styles.DivLinksPlus}>+</div></Link>
+            </div>
         </div>
     )
 }
