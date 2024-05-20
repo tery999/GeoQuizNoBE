efb25ac00055ac43e1a14ef53c64108a79987a9d
tery999 <51335465+tery999@users.noreply.github.com>
2024-05-20 15:56:24 +0300

    css testing styles

--- src/Components/Scores/Scores.jsx
+++ src/Components/Scores/Scores.jsx
@@ -5,17 +5,19 @@ import filterScoreGrouping from "./filterScoreGrouping";
 
 export default function Scores() {
     debugger;
-    const {score} = useContext(ScoreContext)
+    const { score } = useContext(ScoreContext)
     const flagScores = filterScoreGrouping(score, "Flag");
-    console.log(flagScores)
 
     return (
         <div className={styles.container}>
-        Scores Page
-        {Object.entries(score).map( ([ key, value ], i) => <div key={i}>
-            {key} {value}
-        </div>)
-        }
-    </div>
+            <h2>Your scores</h2>
+            <div className={styles.flagGroup}>
+                <h2>Flags</h2>
+                {flagScores.map(([key, value], i) => <div key={i}>
+                  <div>{key}</div>   <div>{value}</div> 
+                </div>)
+                }
+            </div>
+        </div>
     )
 }
